37711d87e60f2b23ab7bffc7426b556ec329adff
aashaykadakia <aashaykadakia@tamu.edu>
2023-01-29 17:11:56 -0600

    reload issue

--- client/src/index.js
+++ client/src/index.js
@@ -3,23 +3,31 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App'
+import Frontpage from './views/Frontpage';
+import Team from './views/Team';
+import ComputerVision from './views/ComputerVision';
+import UserInterface from './views/UserInterface';
 /*import Home from './views/Home/Home';
 import Home2 from './views/Home2/Home2';
 import Home3 from './views/Home3/Home3';*/
 
 
-/*const router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>
+    element: <Frontpage/>
   },
   {
-    path: "/about",
-    element: <Home2/>
+    path: "/team",
+    element: <Team/>
   },
   {
-    path: "/map",
-    element: <Home3/>
+    path: "/computervision",
+    element: <ComputerVision/>
+  },
+  {
+    path: "/userinterface",
+    element: <UserInterface/>
   },
 ])
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -27,14 +35,14 @@ root.render(
   <React.StrictMode>
     <RouterProvider router={router}/>
   </React.StrictMode>
-);*/
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
